d273d14b3df596b5a2d7deaf5d9bc1107838d3c2
John Tarleton Moore <tarletonmoore@gmail.com>
2022-08-27 11:22:26 -0400

    commit tree 0ae4df51c1f1f81d4e14be1d5a07e906e1c75c36

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  attr_reader :side1, :side2, :side3
+
+def initialize(side1, side2, side3)
+@side1 = side1
+@side2 = side2
+@side3 = side3
+
+end
+
+def kind
+check_triangle
+  if side1 == side2 && side2 == side3
+    :equilateral
+  elsif side1 == side2 || side2 == side3 || side1 == side3
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+def check_triangle
+
+real_triangle = [(side1 + side2 > side3), (side1 + side3 > side2), (side2 + side3 > side1)]
+[side1, side2, side3].each do |side|
+  real_triangle << false if side <= 0
+  raise TriangleError if real_triangle.include?(false)
+end
+
+end
+
+class TriangleError < StandardError
+  
+end
+
 end
